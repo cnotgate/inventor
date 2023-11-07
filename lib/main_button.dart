32df7695508370e1fe31e7845bf552a69a76f3b3
cnotgate <49557224+cnotgate@users.noreply.github.com>
2023-11-08 01:29:30 +0700

    dev

---
+++ lib/main_button.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class MainButton extends StatelessWidget {
+  const MainButton({super.key, required this.icon, required this.text, required this.onTap, this.color});
+
+  final IconData? icon;
+  final String text;
+  final Color? color;
+  final Function()? onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: color,
+      child: InkWell(
+        onTap: onTap,
+        child: Container(
+          padding: const EdgeInsets.all(8),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(
+                  icon,
+                  color: Colors.white,
+                  size: 30.0,
+                ),
+                Text(
+                  text,
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(color: Colors.white),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
